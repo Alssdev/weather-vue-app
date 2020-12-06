58f550b548296d2664ce51b08b99746eb68d3281
alssdev <alssdev3@gmail.com>
2020-12-05 19:58:10 -0600

    search bar layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
     <navBar></navBar>
+
+    <!-- search bar -->
+    <section class="section">
+      <div class="field is-grouped">
+        <div class="control is-expanded">
+          <input
+            v-model="city"
+            type="text"
+            name="city"
+            id="city"
+            placeholder="Search your city"
+            class="input is-dark"
+          />
+        </div>
+        <div class="control">
+          <button class="button is-dark">
+            Search
+          </button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -14,7 +35,9 @@ export default {
     navBar,
   },
 
-  data: () => ({}),
+  data: () => ({
+    city: '',
+  }),
 
   methods: {},
 };
