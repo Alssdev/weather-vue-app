5299e27a5ca93f53c0833c28d2678576997f5440
alssdev <alssdev3@gmail.com>
2020-12-07 14:30:52 -0600

    Layout of city cards

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,50 @@
   <div>
     <navBar></navBar>
 
+    <div class="section">
+      <section>
+        <div class="field is-grouped">
+          <div class="control is-expanded">
+            <input
+              v-model="city"
+              type="text"
+              name="city"
+              id="city"
+              placeholder="Search your city"
+              class="input is-dark"
+            />
+          </div>
+          <div class="control">
+            <button class="button is-dark">
+              Search
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <!-- city cards -->
+      <section class="mt-5">
+        <div class="columns is-multiline">
+          <div class="column is-4">
+            <city-card></city-card>
+          </div>
+        </div>
+      </section>
+    </div>
     <!-- search bar -->
-    <section class="section">
-      <div class="field is-grouped">
-        <div class="control is-expanded">
-          <input
-            v-model="city"
-            type="text"
-            name="city"
-            id="city"
-            placeholder="Search your city"
-            class="input is-dark"
-          />
-        </div>
-        <div class="control">
-          <button class="button is-dark">
-            Search
-          </button>
-        </div>
-      </div>
-    </section>
   </div>
 </template>
 
 <script>
-import navBar from '@/components/Navbar';
+import NavBar from '@/components/Navbar';
+import CityCard from '@/components/CityCard';
 
 export default {
   name: 'App',
 
   components: {
-    navBar,
+    navBar: NavBar,
+    'city-card': CityCard,
   },
 
   data: () => ({
---
+++ src/components/CityCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="box">
+    <section class="columns mb-0">
+      <div class="column">
+        <p class="title is-3">Ciudad</p>
+      </div>
+      <div class="column has-text-right">
+        <p class="title is-4">20 °C</p>
+      </div>
+    </section>
+    <p>light intensity drizzle</p>
+
+    <div class="columns mt-1">
+      <div class="column">
+        <p><b>Max Temp:</b> 2 °C</p>
+      </div>
+      <div class="column">
+        <p><b>Min Temp:</b> 2 °C</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
